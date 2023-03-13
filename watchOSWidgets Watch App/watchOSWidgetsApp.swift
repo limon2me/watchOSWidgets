6263ecdc29ad32fe5afcf53f21f52bbeb528c017
Vladimir Khabarov <vkhabarov@alfabank.ru>
2023-03-13 19:25:07 +0800

    Initial commit. Empty project.

---
+++ watchOSWidgets Watch App/watchOSWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSWidgetsApp.swift
+//  watchOSWidgets Watch App
+//
+//  Created by Vladimir Khabarov on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct watchOSWidgets_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchOSWidgets/watchOSWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSWidgetsApp.swift
+//  watchOSWidgets
+//
+//  Created by Vladimir Khabarov on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct watchOSWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
